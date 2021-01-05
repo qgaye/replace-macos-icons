bac3f956df337d765f101472071e0ec4c71d71bf
qgaye <qgaye0601@outlook.com>
2021-01-05 17:31:03 +0800

    support wildcard and update config

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ You need install [fileicon](https://github.com/mklement0/fileicon) like `brew in
 
 Then look for your favorite icons(in [here](https://macosicons.com/)) and store in folder `./icons`
 
-And fill in the config file `replacement.json`, like `[app name]: [icons name in folder]`
+And fill in the config file `replace.config.json`, like `[app name]: [icons name in folder]`
 
 Finally, run `python replace.py` and enjoy it
+
+![screenshot1](./screenshots/screenshot1.png)
+
+![screenshot2](./screenshots/screenshot2.png)
--- replace.py
+++ replace.py
@@ -6,34 +6,75 @@ import json
 import subprocess
 
 icons_floder = "./icons"
-toolbox_install_folder = "~/Library/Application\ Support/JetBrains/Toolbox/apps/"
 
-config = {}
+replace_config = "./replace.config.json"
 
-with open("./replacement.json", "r") as f:
-    config = json.load(f)
+# ~ -> /Users/qgaye
+def handle_expanduser(path):
+    return os.path.expanduser(path)
 
-# TODO: 自动将最新的ToolBox安装的IDE路径配置到replacement.json中
+# 空格 -> \空格
+def handle_escape(path):
+    return path.replace(" ", "\\ ")
 
-for path, value in config.items():
-    path = path.replace(" ", "\\ ")
-    if isinstance(value, str):
-        pass
-    if isinstance(value, dict):
-        for app_name, replacement in value.items():
-            # 路径中空格需要转移 空格 -> \空格
-            app_name_with_escapes = app_name.replace(" ", "\\ ")
-            app_path = f"{path}/{app_name_with_escapes}.app"
-            replacement_path = f"{icons_floder}/{replacement}.icns"
-            print(f"replacing {app_name} icon ...")
+# regex: str
+# paths: [str]
+def handle_regex(paths):
+    has_replaced = False
+    new_paths = paths[:]
+    for regex in paths:
+        start = -1
+        end = -1;
+        for i in range(len(regex)):
+            if regex[i] == '{':
+                start = i
+            if regex[i] == '}' and start != -1:
+                has_replaced = True
+                new_paths.remove(regex)
+                end = i
+                prefix = regex[0 : start]
+                replace_place = regex[start : end + 1] 
+                suffix = regex[end + 1 : len(regex)]
+                solve_replace_place(prefix, replace_place, suffix, new_paths)
+    if has_replaced:
+        return handle_regex(new_paths)
+    return new_paths
 
-            command = f"sudo fileicon set {app_path} {replacement_path}"
-            status, output = subprocess.getstatusoutput(command)
-            if status != 0:
-                print(f"failed to replace icon for {app_name}, err: {output}")
-                print(f"execute command: {command}")
-                sys.exit(0)
+def solve_replace_place(prefix, replace_place, suffix, new_paths):
+    if replace_place[1 : -1] == "*":
+        files = os.listdir(prefix)
+        for file in files:
+            if os.path.isdir(f"{prefix}{file}"):
+                new_paths.append(f"{prefix}{file}{suffix}")
 
-print("finish")
+def execute_replace(app_path, replace_icon):
+    app_path = f"{handle_escape(app_path)}.app"
+    replace_icon_path = f"{icons_floder}/{replace_icon}.icns"
+    print(f"replacing {app_name} icon ...")
+
+    command = f"sudo fileicon set {app_path} {replace_icon_path}"
+    # print(command)
+    status, output = subprocess.getstatusoutput(command)
+    if status != 0:
+        print(f"failed to replace icon for {app_name}, err: {output}")
+        print(f"execute command: {command}")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    config = {}
+
+    with open(replace_config, "r") as f:
+        config = json.load(f)
+
+    for path, value in config.items():
+        if isinstance(value, str):
+            pass
+        if isinstance(value, dict):
+            for app_name, replace_icon in value.items():
+                app_paths = f"{handle_expanduser(path)}/{app_name}"
+                for app_path in handle_regex([app_paths]):
+                    execute_replace(app_path, replace_icon)
+
+    print("finish")
 
 
